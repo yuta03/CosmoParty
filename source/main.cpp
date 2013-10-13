0a7029dddd2a00b549d9ff87f86818819e1c785e
yuta <yuta03x@gmail.com>
2013-10-14 04:12:02 +0900

    Show Player

--- source/main.cpp
+++ source/main.cpp
@@ -16,6 +16,139 @@ using namespace std;
 
 //================================================== ユーティリティ ==================================================
 
+//-------------------- 3D --------------------
+
+/**
+ * 3Dベクトル
+ */
+struct Vector3f : public VECTOR{
+	Vector3f();
+	Vector3f( const VECTOR& rhs );
+	Vector3f( float ax, float ay, float az );
+	~Vector3f();
+	
+	Vector3f& set( float ax, float ay, float az );
+	
+	Vector3f& operator =( const VECTOR& rhs );
+	Vector3f& operator +=( const VECTOR& rhs );
+	
+	Vector3f& setLength( float len );
+	
+	float getLength() const;
+	float getLength2() const;
+};
+
+const Vector3f operator +( const VECTOR& lhs, const VECTOR& rhs );
+const Vector3f operator -( const VECTOR& lhs, const VECTOR& rhs );
+float getDistance( const VECTOR& lhs, const VECTOR& rhs );
+
+/**
+ * 生成
+ */
+Vector3f::Vector3f(){
+	x = 0.0f;
+	y = 0.0f;
+	z = 0.0f;
+}
+/**
+ * 生成(コピー)
+ */
+Vector3f::Vector3f( const VECTOR& rhs ){
+	x = rhs.x;
+	y = rhs.y;
+	z = rhs.z;
+}
+/**
+ * 生成
+ */
+Vector3f::Vector3f( float _x, float _y, float _z ){
+	x = _x;
+	y = _y;
+	z = _z;
+}
+/**
+ * 破棄
+ */
+Vector3f::~Vector3f(){
+}
+/**
+ * 値変更
+ */
+Vector3f& Vector3f::set( float _x, float _y, float _z ){
+	x = _x;
+	y = _y;
+	z = _z;
+	return *this;
+}
+/**
+ * コピー
+ */
+Vector3f& Vector3f::operator =( const VECTOR& rhs ){
+	x = rhs.x;
+	y = rhs.y;
+	z = rhs.z;
+	return *this;
+}
+/**
+ * 加算
+ */
+Vector3f& Vector3f::operator +=( const VECTOR& rhs ){
+	x += rhs.x;
+	y += rhs.y;
+	z += rhs.z;
+	return *this;
+}
+/**
+ * 長さ変更
+ */
+Vector3f& Vector3f::setLength( float len ){
+	float oriLen = getLength();
+	if ( oriLen < FLT_EPSILON ) return *this;
+	float v = len / oriLen;
+	
+	x = x * v;
+	y = y * v;
+	z = z * v;
+	
+	return *this;
+}
+/**
+ * 長さ取得
+ */
+float Vector3f::getLength() const{
+	return ( float )sqrt( getLength2() );
+}
+/**
+ * 長さの２乗取得
+ */
+float Vector3f::getLength2() const{
+	return x * x + y * y + z * z;
+}
+
+/**
+ * 加算
+ */
+const Vector3f operator +( const VECTOR& lhs, const VECTOR& rhs ){
+	return Vector3f( lhs.x + rhs.x,  lhs.y + rhs.y, lhs.z + rhs.z );
+}
+/**
+ * 減算
+ */
+const Vector3f operator -( const VECTOR& lhs, const VECTOR& rhs ){
+	return Vector3f( lhs.x - rhs.x,  lhs.y - rhs.y, lhs.z - rhs.z );
+}
+/**
+ * 
+ */
+float getDistance( const VECTOR& lhs, const VECTOR& rhs ){
+	float x = lhs.x - rhs.x;
+	float y = lhs.y - rhs.y;
+	float z = lhs.z - rhs.z;
+	return ( float )sqrt( x * x + y * y + z * z );
+}
+
+static const float F_PI = ( float )M_PI; /** float 型のπ */
+
 //-------------------- キー --------------------
 
 /**
@@ -110,6 +243,7 @@ public:
  */
 enum SceneCode{
 	SceneCodeEmpty4Develop = 0, /** 【開発用】空シーン */
+	SceneCodePlayerControll4Develop, /** 【開発用】プレイヤー操作シーン */
 };
 
 /**
@@ -129,7 +263,7 @@ struct Global{
  */
 Global::Global():
 keypad1(),
-scene( NULL ), sceneCode( SceneCodeEmpty4Develop ){
+scene( NULL ), sceneCode( SceneCodePlayerControll4Develop ){
 }
 
 /**
@@ -185,6 +319,100 @@ void EmptyScene::onDraw(){
 
 
 
+//================================================== 【開発用】プレイヤー操作シーン ==================================================
+
+
+/**
+ * カメラ情報
+ */
+struct Camera{
+	Vector3f position; /** 位置 */
+	Vector3f target; /** 対象 */
+};
+
+/**
+ * プレイヤー情報
+ */
+struct Player{
+	Vector3f position; /** 位置 */
+};
+
+/**
+ * プレイヤー操作シーン
+ */
+class PlayerControllScene : public virtual Scene{
+public:
+	PlayerControllScene( Global* g );
+	~PlayerControllScene();
+	
+	void onUpdate();
+	void onDraw();
+
+private:
+	Global* g_; /** グローバルオブジェクト */
+	int modelId_; /** モデル ID */
+	
+	Player player_; /** プレイヤー */
+	Camera camera_; /** カメラ */
+};
+
+/**
+ * 生成
+ */
+PlayerControllScene::PlayerControllScene( Global* g ):
+Scene(),
+g_( g ),
+modelId_( -1 ),
+player_(), camera_(){
+	// モデル読み込み
+	modelId_ = MV1LoadModel( "resource/airplane_player.x" );
+}
+/**
+ * 破棄
+ */
+PlayerControllScene::~PlayerControllScene(){
+	// モデル破棄
+	MV1DeleteModel( modelId_ ); modelId_ = -1;
+}
+
+/**
+ * 更新
+ */
+void PlayerControllScene::onUpdate(){
+	// プレイヤ
+	player_.position.set( 0.0f, 0.0f, 0.0f );
+	
+	// カメラ設定
+	camera_.target = player_.position;
+	camera_.position = camera_.target + Vector3f( 0.0f, 1.0f, -6.0f );
+}
+
+/**
+ * 描画
+ */
+void PlayerControllScene::onDraw(){
+	ClearDrawScreen(); // クリア
+	
+	// カメラ
+	{
+		SetupCamera_Perspective( F_PI * 30.f / 180.f ); // 画角
+		SetCameraNearFar( 1.0f, 1000.0f ); // 前面・背面の距離
+		
+		SetCameraPositionAndTarget_UpVecY( camera_.position, camera_.target ); // カメラ姿勢
+	}
+	
+	// プレイヤー描画
+	{
+		MV1SetPosition( modelId_, player_.position );
+		MV1SetScale( modelId_, Vector3f( 0.0075f, 0.0075f, 0.0075f ) );
+		
+		MV1DrawModel( modelId_ ); // 描画
+	}
+	
+	ScreenFlip(); // 描画完了
+}
+
+
 
 
 
@@ -197,6 +425,7 @@ void EmptyScene::onDraw(){
  */
 static Scene* createScene( SceneCode code, Global* g ){
 	if ( code == SceneCodeEmpty4Develop ) return new EmptyScene( g );
+	if ( code == SceneCodePlayerControll4Develop ) return new PlayerControllScene( g );
 	
 	return NULL;
 }
